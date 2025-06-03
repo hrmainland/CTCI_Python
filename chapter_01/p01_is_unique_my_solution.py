05141dafa40c44202fcc6f3fa295a086736b43d0
hrmainland <hrrmainland@gmail.com>
2025-06-03 14:49:27 +1000

    q1.1

--- chapter_01/p01_is_unique_my_solution.py
+++ chapter_01/p01_is_unique_my_solution.py
@@ -1,1 +1,13 @@
 # My solution to p01_is_unique.py
+
+def is_unique(input: str):
+    my_set = set()
+    for char in input:
+        if char in my_set:
+            return False
+        my_set.add(char)
+    return True
+
+    
+
+
