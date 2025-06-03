b1e4ceacfa435756f8491125b1c297d8b87f9dde
hrmainland <hrrmainland@gmail.com>
2025-06-03 15:28:53 +1000

    q2-3

--- chapter_01/p02_check_permutation_my_solution.py
+++ chapter_01/p02_check_permutation_my_solution.py
@@ -1,1 +1,38 @@
 # My solution to p02_check_permutation.py
+from collections import Counter
+import unittest
+
+
+def is_permutation(str_a: str, str_b: str):
+    freq_a = Counter(str_a)
+    freq_b = Counter(str_b)
+    return freq_a == freq_b
+
+
+class Test(unittest.TestCase):
+    # str1, str2, is_permutation
+    test_cases = (
+        ("dog", "god", True),
+        ("abcd", "bacd", True),
+        ("3563476", "7334566", True),
+        ("wef34f", "wffe34", True),
+        ("dogx", "godz", False),
+        ("abcd", "d2cba", False),
+        ("2354", "1234", False),
+        ("dcw4f", "dcw5f", False),
+        ("DOG", "dog", False),
+        ("dog ", "dog", False),
+        ("aaab", "bbba", False),
+    )
+
+    testable_functions = [is_permutation]
+
+    def test_cp(self):
+        # true check
+        for check_permutation in self.testable_functions:
+            for str1, str2, expected in self.test_cases:
+                self.assertEqual(check_permutation(str1, str2), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- chapter_01/p03_urlify_my_solution.py
+++ chapter_01/p03_urlify_my_solution.py
@@ -1,1 +1,32 @@
+import unittest
+
+
 # My solution to p03_urlify.py
+def urlify(input: str, length: int):
+    return "%20".join([elem for elem in input[:length].split(" ") if elem])
+
+
+def correct(input, length):
+    return input[:length].replace(" ", "%20")
+
+
+class Test(unittest.TestCase):
+    """Test Cases"""
+
+    test_cases = {
+        ("much ado about nothing      ", 22): "much%20ado%20about%20nothing",
+        ("Mr John Smith       ", 13): "Mr%20John%20Smith",
+        (" a b    ", 4): "%20a%20b",
+        (" a b       ", 5): "%20a%20b%20",
+    }
+    testable_functions = [correct]
+
+    def test_urlify(self):
+        for urlify in self.testable_functions:
+            for args, expected in self.test_cases.items():
+                actual = urlify(*args)
+                assert actual == expected, f"Failed {urlify.__name__} for: {[*args]}"
+
+
+if __name__ == "__main__":
+    unittest.main()
